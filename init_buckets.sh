7ca93c82671c059352dcc2bbd51e75a80785acd5
Julian Kirstein <juliankirstein37@gmail.com>
2024-12-08 03:35:02 +0100

    first commit

---
+++ init_buckets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Ensure mc is installed
+if ! command -v mc &> /dev/null
+then
+  echo "mc could not be found, installing..."
+  wget https://dl.min.io/client/mc/release/linux-amd64/mc
+  chmod +x mc
+  mv mc /usr/bin/
+fi
+mc alias set myminio http://127.0.0.1:9000 minioadmin minioadmin
+mc mb myminio/amgt-alerts
+mc mb myminio/loki-blocks
+mc mb myminio/loki-ruler
+mc mb myminio/mimir-blocks
+mc mb myminio/mimir-ruler
+
+# Verify the configuration by listing buckets
+mc ls myminio
